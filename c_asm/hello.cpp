b8e736ffe8703e14417ea4b55a16b184b2b4a61b
onixldlc <81515667+onixldlc@users.noreply.github.com>
2022-08-22 02:10:25 +0800

    Update hello.cpp

--- c_asm/hello.cpp
+++ c_asm/hello.cpp
@@ -1,12 +1,15 @@
-// only works on linux (with windows you need different interupt)
+// only works on linux (becs i have no clue how this work :V and cant even compile this on my windows machine)
+char helloWrld[] = "hello world!\n";
+char* helloP = &helloWrld[0];
+
 int main() {
     asm(
-        "mov $4, %%eax;"
-        "mov $1, %%ebx;"
-        "mov $13, %%edx;"
-        "int $0x80;"
-        :
-        :"c"("hello world!")
-       );
+        "hello:;"
+        "mov $4, %eax;"
+        "mov $1, %ebx;"
+        "mov helloWrld, %ecx;"
+        "mov $13, %edx;"
+        "int $0x80"
+    );
     return 0;
 }
